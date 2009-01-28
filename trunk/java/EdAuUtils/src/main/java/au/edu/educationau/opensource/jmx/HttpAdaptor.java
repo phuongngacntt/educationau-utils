9a74ebb99d5957ceff3c39991bfe7ae4b468aebb
bslattery@educationau.edu.au <bslattery@educationau.edu.au@28a02ff7-6743-0410-ade2-f97035505400>
2009-01-28 02:58:04 +0000

    Renamed HttpAdapter to HttpAdaptor

--- trunk/java/EdAuUtils/src/main/java/au/edu/educationau/opensource/jmx/HttpAdaptor.java
+++ trunk/java/EdAuUtils/src/main/java/au/edu/educationau/opensource/jmx/HttpAdaptor.java
@@ -1,13 +1,11 @@
 package au.edu.educationau.opensource.jmx;
 
-import mx4j.tools.adaptor.http.HttpAdaptor;
-
 import org.springframework.jmx.export.annotation.ManagedResource;
 
 /**
  * Needed to extend underlying HttpAdapter to add the annotation, otherwise Spring init errors...
  */
-@ManagedResource(objectName = "bean:name=au.edu.educationau.myedna.jmx.HttpAdapter")
-public class HttpAdapter extends HttpAdaptor {
+@ManagedResource(objectName = "bean:name=au.edu.educationau.opensource.jmx.HttpAdaptor")
+public class HttpAdaptor extends mx4j.tools.adaptor.http.HttpAdaptor {
 
 }
